0de88037cb3a7d39d8dd69f3bda6f17315ff5952
Thomas Maguire <thomas@maguire.email>
2018-09-16 17:31:04 +0100

    Bigger logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="IT Tom Tips" style={{ width: '88px' }} />
+            <img src={logo} alt="IT Tom Tips" style={{ width: '150px' }} />
           </figure>
         </Link>
       </div>
